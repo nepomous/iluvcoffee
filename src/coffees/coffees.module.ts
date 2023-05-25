f7274fd5f5e00c92323018d776233a1d99cf340d
Fillipe Oliveira <fillipe.oliveira@hingehealth.com>
2023-05-24 23:05:39 -0300

    create database connections

---
+++ src/coffees/coffees.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CoffeesController } from './coffees.controller';
+import { CoffeesService } from './coffees.service';
+import { Coffee } from './entities/coffee.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Coffee])], // 👈 Adding Coffee Entity here to TypeOrmModule.forFeature
+  controllers: [CoffeesController],
+  providers: [CoffeesService],
+})
+export class CoffeesModule {}
